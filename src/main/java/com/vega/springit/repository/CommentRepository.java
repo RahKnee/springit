ebb9a39766f65495d18c57a29a948cae6f4897f5
Ronnie Phelps <ronnie.phelps@fiserv.com>
2019-03-22 16:28:34 -0400

    initial repositories for link and comment

--- src/main/java/com/vega/springit/repository/CommentRepository.java
+++ src/main/java/com/vega/springit/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.vega.springit.repository;
 
-public interface CommentRepository {
+import com.vega.springit.domain.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comments,Long> {
 }
--- src/main/java/com/vega/springit/repository/LinkRepository.java
+++ src/main/java/com/vega/springit/repository/LinkRepository.java
@@ -1,4 +1,8 @@
 package com.vega.springit.repository;
 
-public interface LinkRepository {
+import com.vega.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link,Long> {
+
 }
