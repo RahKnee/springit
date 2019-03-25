42c0c776b9302e8168cd85cdf707bdf0768bfb1c
Ronnie Phelps <ronnie.phelps@fiserv.com>
2019-03-25 16:29:55 -0400

    Setting up our in memory database

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,24 +1,34 @@
 package com.vega.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+//@Entity
+//@Data
+//@NoArgsConstructor
 @Entity
-@Data
+@RequiredArgsConstructor
+@Getter @Setter
+@ToString
 @NoArgsConstructor
 public class Comment extends Auditable{
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String body;
 
     //link
+    @NonNull
     @ManyToOne
     private Link link;
 
+//    public Comment(@NonNull String body, @NonNull Link link) {
+//        this.body = body;
+//        this.link = link;
+//    }
 }
--- src/main/java/com/vega/springit/domain/Link.java
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -1,7 +1,6 @@
 package com.vega.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,16 +10,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@RequiredArgsConstructor
+@Getter @Setter
+@ToString
 @NoArgsConstructor
-@Data
 public class Link extends Auditable {
     @Id
     @GeneratedValue
     private  Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String url;
 
     // comments
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<Comment>();
+
+//    public Link(@NonNull String title, @NonNull String url) {
+//        this.title = title;
+//        this.url = url;
+//    }
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
